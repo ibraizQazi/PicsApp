9a62e0c5b7378c3e00e3c99d8fefbb01eaf14610
Ibraiz <ibraizqazi@gmail.com>
2022-12-15 20:31:58 +0500

    Initial Commit

---
+++ ObjectRemover/ObjectRemoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectRemoverApp.swift
+//  ObjectRemover
+//
+//  Created by Ibraiz Qazi on 15/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct ObjectRemoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
