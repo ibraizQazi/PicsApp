8025e151e4f59d13458b60e02fddfe2b194328eb
Ibraiz <ibraizqazi@gmail.com>
2024-01-26 18:53:52 +0500

    Initial Commit

---
+++ PicEditApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PicEditApp
+//
+//  Created by Ibraiz Qazi on 26/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PicEditApp/PicEditAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicEditAppApp.swift
+//  PicEditApp
+//
+//  Created by Ibraiz Qazi on 26/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct PicEditAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
